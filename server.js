f69653882b5e595147e99128c52c4183b6caf313
Goran Tubić <mail.goran96@gmail.com>
2020-02-11 13:59:16 +0100

    Serving on every route

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const path = require("path");
 let port = 5675;
 const app = express();
 
-app.use(express.static("public"));
-// app.get("/", (req, res) => {
-//   res.status(200).send("WHATABYTE: Food For Devs");
-// });
+// app.use(express.static("public"));
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+})
 
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
